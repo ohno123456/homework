aae767ce608f08773cb81ff6c0044e30835ea0d6
= <=>
2019-10-25 01:33:43 +0800

    add vuev2 2019-10-25

---
+++ vuejs/CLI/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <div> i am a cmp, {{ msg }}</div>
+</template>
+
+<script>
+export default {
+    data () {
+        return{
+            msg:"hello world!"
+            }
+        
+    }
+}
+</script>
+
+<style>
+
+div {
+    background-color : red;
+}
+</style>
